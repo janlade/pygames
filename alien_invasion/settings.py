a9e402d48f42aab3e3d1e61240daadb8c51a2580
Jan Lade <jan.lade@jedox.com>
2022-12-12 14:28:34 +0100

    update

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -23,5 +23,5 @@ class Settings:
         self.bullet_speed = 0.5
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (0, 255, 0)
+        self.bullet_color = (255, 0, 0)
         self.bullets_allowed = 3
