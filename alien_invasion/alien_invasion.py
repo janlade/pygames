46c37680ab6d4dbc882a40948699471dc7355b49
Jan Lade <jan.lade@jedox.com>
2022-12-13 10:49:19 +0100

    update

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -43,6 +43,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
 
@@ -94,7 +95,14 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
-        # print(len(self.bullets))  
+        # print(len(self.bullets))
+        
+        
+    def _update_aliens(self):
+        """Check if the fleet is at an edge,
+            then update the positions of all aliens in the fleet."""
+        self._check_fleet_edges()
+        self.aliens.update()
 
             
     def _update_screen(self):
@@ -108,7 +116,6 @@ class AlienInvasion:
         pygame.display.flip()
             
     
-
     def _create_fleet(self):
         """Create the fleet of aliens."""
         # Create an alien and find the number of aliens in a row.
@@ -128,6 +135,7 @@ class AlienInvasion:
         for row_number in range(number_rows):
             for alien_number in range(number_aliens_x):
                 self._create_alien(alien_number, row_number)
+   
             
     def _create_alien(self, alien_number, row_number):
         """Create an alien and place it in the row."""
@@ -137,6 +145,21 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+    
+    
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
         
 if __name__ == '__main__':
     # Make a game instance, and run the game
